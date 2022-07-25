fe3f2c72abc563784bc39b7de3898d06d0ec29fd
Jiaxin Yu <9447954+JiaxinYu@users.noreply.github.com>
2022-07-25 17:54:32 +0800

    Update preproc01.py
    adding 2019 and before dataset, merged with 2020 and 2021 dataset

--- preproc01.py
+++ preproc01.py
@@ -17,6 +17,29 @@ df_lvx = pd.read_csv(rootpath/'cmuh/Levofloxacin.csv')
 df_caz = pd.read_csv(rootpath/'cmuh/Ceftazidime.csv')
 df_sxt = pd.read_csv(rootpath/'cmuh/Trimethoprim-Sulfamethoxazole.csv')
 
+
+## 2018-2019
+directory = Path(path-to-file)
+df_filepath = pd.read_csv(directory/'mbt_0318.csv')
+# drop QC files
+df_filepath.drop(df_filepath[df_filepath['accnum'].str.len() < 10].index, axis=0, inplace=True)
+df_filepath.reset_index(drop=True, inplace=True)
+# extract accnum
+temp_test = df_filepath['accnum'].str.split('#', expand=True)
+df_filepath = pd.concat([df_filepath, temp_test], 1)
+df_filepath.columns = ['date', 'accum', 'Accession #', 'case #']
+# drop more than one isolates in a single accnum
+df_filepath.drop_duplicates(subset=['Accession #'], keep=False, inplace=True)
+# # include dataset before 2020
+df_filepath['date'] = pd.to_numeric(df_filepath['date'], errors='coerce', downcast="integer")
+df_filepath.dropna(subset=['date'], inplace=True)
+df_filepath.drop(df_filepath[df_filepath['date'] > 109000000].index, 0, inplace=True)
+df_filepath.reset_index(drop=True, inplace=True)
+df_filepath.to_csv(rootpath/'decoding_18_19.csv', index=False)
+df_filepath
+
+
+## 2020-2021
 IndexDir = []
 directory = Path(path-to-file)
 for fidx in os.listdir(directory):
@@ -68,10 +91,29 @@ def mergedata(df1, df2):
     return temp_df1
   
   
+## 2020 and 2021
 min_df1 = mergedata(df_mi, decoding_df)
 lvx_df1 = mergedata(df_lvx, decoding_df)
 caz_df1 = mergedata(df_caz, decoding_df)
 sxt_df1 = mergedata(df_sxt, decoding_df)
+
+
+## 2019 and before
+min_df2 = mergedata(df_mi, df_filepath)
+lvx_df2 = mergedata(df_lvx, df_filepath)
+caz_df2 = mergedata(df_caz, df_filepath)
+sxt_df2 = mergedata(df_sxt, df_filepath)
+
+for idx in [min_df2, lvx_df2, caz_df2, sxt_df2]:
+    idx['foldername'] = idx['date'].astype('int')
+    idx['filename'] = idx['accum']
+    
+    
+## merge all 4 years
+min_df0 = pd.concat([min_df1, min_df2], axis=0, join='inner')
+lvx_df0 = pd.concat([lvx_df1, lvx_df2], axis=0, join='inner')
+caz_df0 = pd.concat([caz_df1, caz_df2], axis=0, join='inner')
+sxt_df0 = pd.concat([sxt_df1, sxt_df2], axis=0, join='inner')
 
 
 def getfileaccnum(rootpath):
@@ -90,38 +132,50 @@ def getfileaccnum(rootpath):
                 .str.split('/', expand=True)
     return pd.concat([path_all, datapath.iloc[:, 1:3]], 1)
   
-  
-check01 = pd.DataFrame([])
-for pathidx in [file-to-path01, file-to-path02]:
-    check01 = pd.concat([check01, getfileaccnum(pathidx)], 0)
+## 2019 and before
+check2019 = getfileaccnum(Path(file-to-path00))
+check2019.columns = ['fullpath', 'foldername', 'filename']
+check2019.drop(check2019[check2019['filename'].str.len() < 10].index, axis=0, inplace=True)
+check2019.reset_index(drop=True, inplace=True)
 
-check01.columns = ['fullpath', 'foldername', 'filename']
-check01
+
+## 2020 and 2021
+check2021 = pd.DataFrame([])
+for pathidx in [file-to-path01, file-to-path02]:
+    check2021 = pd.concat([check2021, getfileaccnum(pathidx)], 0)
+
+check2021.columns = ['fullpath', 'foldername', 'filename']
+
+
+## merge all fullpath
+check18_21 = pd.concat([check2019, check2021], 0)
+
+## save fullpath file to csv
+check18_21.to_csv(rootpath/'msdata_fullpath18to21_25jul22.csv', index=False)
 
 
 ## min
-min_df2 = min_df1.merge(check01, how='inner', on=['foldername', 'filename'])
-min_df2.drop_duplicates(subset=['Accession #'], inplace=True)
-min_df2.reset_index(drop=True, inplace=True)
+min_df =  min_df0.astype('str').merge(check18_21, how='inner', on=['foldername', 'filename'])
+min_df.drop_duplicates(subset=['Accession #'], inplace=True)
+min_df.reset_index(drop=True, inplace=True)
 
 ## lvx
-lvx_df2 = lvx_df1.merge(check01, how='inner', on=['foldername', 'filename'])
-lvx_df2.drop_duplicates(subset=['Accession #'], inplace=True)
-lvx_df2.reset_index(drop=True, inplace=True)
+lvx_df = lvx_df0.astype('str').merge(check18_21, how='inner', on=['foldername', 'filename'])
+lvx_df.drop_duplicates(subset=['Accession #'], inplace=True)
+lvx_df.reset_index(drop=True, inplace=True)
 
 ## caz
-caz_df2 = caz_df1.merge(check01, how='inner', on=['foldername', 'filename'])
-caz_df2.drop_duplicates(subset=['Accession #'], inplace=True)
-caz_df2.reset_index(drop=True, inplace=True)
+caz_df = caz_df0.astype('str').merge(check18_21, how='inner', on=['foldername', 'filename'])
+caz_df.drop_duplicates(subset=['Accession #'], inplace=True)
+caz_df.reset_index(drop=True, inplace=True)
 
 ## sxt
-sxt_df2 = sxt_df1.merge(check01, how='inner', on=['foldername', 'filename'])
-sxt_df2.drop_duplicates(subset=['Accession #'], inplace=True)
-sxt_df2.reset_index(drop=True, inplace=True)
+sxt_df = sxt_df0.astype('str').merge(check18_21, how='inner', on=['foldername', 'filename'])
+sxt_df.drop_duplicates(subset=['Accession #'], inplace=True)
+sxt_df.reset_index(drop=True, inplace=True)
 
 
-check01.to_csv(rootpath/'msdata_fullpath_18jul22.csv', index=False)
-min_df2.to_csv(rootpath/'sm_min_clean18jul22.csv', index=False)
-lvx_df2.to_csv(rootpath/'sm_lvx_clean18jul22.csv', index=False)
-caz_df2.to_csv(rootpath/'sm_caz_clean18jul22.csv', index=False)
-sxt_df2.to_csv(rootpath/'sm_sxt_clean18jul22.csv', index=False)
+min_df.to_csv(rootpath/'sm_min_clean25jul22.csv', index=False)
+lvx_df.to_csv(rootpath/'sm_lvx_clean25jul22.csv', index=False)
+caz_df.to_csv(rootpath/'sm_caz_clean25jul22.csv', index=False)
+sxt_df.to_csv(rootpath/'sm_sxt_clean25jul22.csv', index=False)
